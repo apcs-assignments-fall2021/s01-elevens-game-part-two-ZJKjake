64ae9f43b2bc610e506f371b81bb49573c468d2f
jakezhang <24jzhang@athenian.org>
2022-01-09 11:45:39 -0800

    done

--- src/main/java/ElevensBoard.java
+++ src/main/java/ElevensBoard.java
@@ -9,11 +9,11 @@ public class ElevensBoard extends Board {
 
     // These three arrays will be used in the constructor:
     // The ranks of the cards for this game to be sent to the deck
-    private static final String[] RANKS = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
+    private static final String[] RANKS = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
     // The suits of the cards for this game to be sent to the deck.
-    private static final String[] SUITS = { "spades", "hearts", "diamonds", "clubs" };
+    private static final String[] SUITS = {"spades", "hearts", "diamonds", "clubs"};
     // The values of the cards for this game to be sent to the deck.
-    private static final int[] POINT_VALUES = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0 };
+    private static final int[] POINT_VALUES = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
 
     // Creates a new ElevensBoard instance with a standard 52 card deck
     public ElevensBoard() {
@@ -24,7 +24,14 @@ public class ElevensBoard extends Board {
     // returns true if **any** two of the given cards adds up to exactly 11
     // and false otherwise
     public boolean containsPairSum11(ArrayList<Integer> cardIndexes) {
-        // YOUR CODE HERE
+        for (int i = 0; i < cardIndexes.size(); i++) {
+            for (int j = i + 1; j < cardIndexes.size(); j++) {
+
+                if (cardAt(cardIndexes.get(i)).getPointValue() + cardAt(cardIndexes.get(j)).getPointValue() == 11) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -32,8 +39,20 @@ public class ElevensBoard extends Board {
     // returns true if there is at least 1 Jack, at least 1 Queen, and at least 1 King
     // amongst the selected cards, and false otherwise
     public boolean containsJQK(ArrayList<Integer> cardIndexes) {
-        // YOUR CODE HERE
-        return false;
+        int J = 0;
+        int Q = 0;
+        int K = 0;
+        for (int i = 0; i < cardIndexes.size(); i++) {
+            String rank = (cardAt(cardIndexes.get(i))).getRank();
+            if (rank.equals("jack")) {
+                J += 1;
+            } else if (rank.equals("queen")) {
+                Q += 1;
+            } else if (rank.equals("king")) {
+                K += 1;
+            }
+        }
+        return (J > 0 && Q > 0 && K > 0);
     }
 
     // Determine if there are any legal plays left on the board.
@@ -47,7 +66,8 @@ public class ElevensBoard extends Board {
 
         // YOUR CODE HERE
         // Just 1-2 lines of code needed
-        return false;
+
+        return containsJQK(allCards) || containsPairSum11(allCards);
     }
 
     // Determines if the selected cards form a valid group for removal. In Elevens,
@@ -55,7 +75,25 @@ public class ElevensBoard extends Board {
     // (2) a group of three cards consisting of a jack, a queen, and a king in some order
     @Override
     public boolean isLegal(ArrayList<Integer> selectedCards) {
-        // YOUR CODE HERE
-        return false;
+//        ArrayList<Integer> allCards = getAllCardIndexes();
+//        for (int i = 0; i < selectedCards.size(); i++) {
+//            for (int j = 0; j < allCards.size(); j++) {
+//                if (allCards.get(j) == selectedCards.get(i)) {
+//                    allCards.remove(j);
+//                    break;
+//                }
+//            }
+//        }
+
+        for (int x:selectedCards){
+            System.out.println(cardAt(x).getRank());
+            System.out.println(cardAt(x).getPointValue());
+        }
+
+        System.out.println(containsJQK(selectedCards));
+        System.out.println(containsPairSum11(selectedCards));
+        System.out.println("HHHHH");
+        return containsJQK(selectedCards)&&selectedCards.size()==3 || containsPairSum11(selectedCards)&&selectedCards.size()==2;
     }
 }
+
